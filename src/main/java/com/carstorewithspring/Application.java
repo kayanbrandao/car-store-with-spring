31001ae4f52de396c21b494a4de6059b30b480e3
kayanbrandao <kayanbrandao.krb@gmail.com>
2023-09-16 19:33:29 -0400

    style: by changing the name of the application file.

--- src/main/java/com/carstorewithspring/Application.java
+++ src/main/java/com/carstorewithspring/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CarStoreWithSpringApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CarStoreWithSpringApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
--- src/test/java/com/carstorewithspring/ApplicationTests.java
+++ src/test/java/com/carstorewithspring/ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CarStoreWithSpringApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {

274f97339617c2958badd4c941d9a0d539f69495
kayanbrandao <kayanbrandao.krb@gmail.com>
2023-09-17 19:05:53 -0400

    refactor: refactoring the control and service of the car entity.

--- src/main/java/com/carstorewithspring/data/dto/CarDto.java
+++ src/main/java/com/carstorewithspring/data/dto/CarDto.java
@@ -1,18 +1,16 @@
-package com.carstorewithspring.data.request;
+package com.carstorewithspring.data.dto;
 
 import com.carstorewithspring.data.enums.GearboxType;
 import com.carstorewithspring.data.model.Model;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
 import java.time.Year;
 
-public record CarRequest(
-        @NotBlank String color,
-        @NotBlank String version,
+public record CarDto(
+        Long id,
+        @NotEmpty String color,
+        @NotEmpty String version,
         GearboxType gearbox,
         @NotNull Year year,
         @NotNull Model model
